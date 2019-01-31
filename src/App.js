b1b68d6f0c4eee3784c21f833a81faf382674d63
LAPTOP-Q7OOIVSO\grace <gracewalkuski@gmail.com>
2019-01-30 17:21:11 -0700

    added documentation links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>App</h1>
+        <a href="https://github.com/ESOF-423/Bindex/blob/master/Documentation/UserDocumentation.md">User Documentation</a>
+        <br />
+        <a href="https://github.com/ESOF-423/Bindex/blob/master/Documentation/DeveloperDocumentation.md">Developer Documentation</a>
       </div>
     );
   }
